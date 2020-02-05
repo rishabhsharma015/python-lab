7edd5ae4167928cac5bfec4ebd27761fb9b6408d
rishabhsharma015 <60418660+rishabhsharma015@users.noreply.github.com>
2020-02-05 11:16:21 +0530

    info

---
+++ shop.py
@@ -0,0 +1,14 @@
+lst = []
+while(1):
+    item = input("enter the item")
+    lst.append(item)
+    
+    n = input("Do you want to continue Y/N")
+    if n.lower()=="n":
+              break
+    else:
+         continue
+print(lst)
+        
+     
+    
